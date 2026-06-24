e74e16e06af24f0808d7dc3c52254950cffd43a4
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2026-06-24 18:38:37 -0300

    Update Shift1.py

--- Shift1.py
+++ Shift1.py
@@ -1,23 +1,23 @@
-class ShiftAnd:
-  __slots__ = "T", "P", "L", "masks", "match", "rmasks"
+class ShiftLeft:
+  __slots__ = "text", "pattern", "masks", "table", "match"
 
-  def __init__(self, texto: str, padrão: str):
-    self.T, self.P, self.L = texto, padrão, len(padrão)
-    self.calculate(); self.execute()
-    print(f"Tabela das máscaras: {self.rmasks}")
+  def __init__(self, text: str, pattern: str):
+    self.text, self.pattern = text, pattern
+    self.calculate_masks(); self.shift_left()
+    print(f"Tabela das máscaras: {self.table}")
     print(f"Matchs encontrados: {self.match}")
 
-  def calculate(self):
-    self.masks = {char: 0 for char in set(self.P)}
-    for i, char in enumerate(reversed(self.P)): self.masks[char] |= 1 << i
-    self.rmasks = ", ".join(f"{m}: {v:b}" for m, v in self.masks.items())
+  def calculate_masks(self):
+    self.masks = {char: 0 for char in set(self.pattern)}
+    for i, char in enumerate(self.pattern): self.masks[char] |= (1 << i)
+    self.table = ", ".join(f"{m}: {v:b}" for m, v in self.masks.items())
 
-  def execute(self):
-    def execute_core():
-      R, OR = 0, 1 << (self.L - 1)
-      for i, char in enumerate(self.T):
-        R = ((R >> 1) | OR) & self.masks.get(char, 0)
-        if R & 1: yield f"no índice {i}"
-    self.match = ", ".join(execute_core())
+  def shift_left(self):
+    def shift_left_core():
+      R, C = 0, 1 << (len(self.pattern) - 1)
+      for i, char in enumerate(self.text):
+        R = self.masks.get(char, 0) & ((R << 1) | 1)
+        if R & C: yield f"no índice {i}"
+    self.match = ", ".join(shift_left_core())
 
-ShiftAnd("os testes testam os alunos com um teste de capacidade", "teste")
+ShiftLeft("os testes testam os alunos com um teste de capacidade", "teste")
