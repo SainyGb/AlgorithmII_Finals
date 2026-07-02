3b6f812ec0ac3ec8b359e0336cb20c508f91731a
Keanu Sales <100793959+keanusales@users.noreply.github.com>
2026-07-01 23:05:34 -0300

    Update Shift1.py

--- Shift1.py
+++ Shift1.py
@@ -5,7 +5,7 @@ class ShiftLeft:
     self.text, self.pattern = text, pattern
     self.calculate_masks(); self.shift_left()
     print(f"Tabela das máscaras: {self.table}")
-    print(f"Matchs encontrados: {self.match}")
+    print(f"Matchs nos índices: {self.match}")
 
   def calculate_masks(self):
     self.masks = dict.fromkeys(self.pattern, 0)
@@ -13,11 +13,8 @@ class ShiftLeft:
     self.table = ", ".join(f"{m}: {v:b}" for m, v in self.masks.items())
 
   def shift_left(self):
-    def shift_left_core():
-      R, C = 0, 1 << (len(self.pattern) - 1)
-      for i, char in enumerate(self.text):
-        R = self.masks.get(char, 0) & ((R << 1) | 1)
-        if R & C: yield f"no índice {i}"
-    self.match = ", ".join(shift_left_core())
+    R, C = 0, 1 << (len(self.pattern) - 1)
+    self.match = ", ".join(str(i) for i, char in enumerate(self.text)
+      if (R := self.masks.get(char, 0) & ((R << 1) | 1)) & C)
 
 ShiftLeft("os testes testam os alunos com um teste de capacidade", "teste")
